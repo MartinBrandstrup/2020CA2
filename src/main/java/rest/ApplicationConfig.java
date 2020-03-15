3305a1cc08b828d7a7a8f3e757941c509b0214e3
MartinBrandstrup <martin.l.brandstrup@gmail.com>
2020-03-15 13:07:05 +0100

    Added and renamed all rest classes

--- src/main/java/rest/ApplicationConfig.java
+++ src/main/java/rest/ApplicationConfig.java
@@ -22,6 +22,11 @@ public class ApplicationConfig extends Application {
     private void addRestResourceClasses(Set<Class<?>> resources) {
         resources.add(depricated.RenameMeResource.class);
         resources.add(org.glassfish.jersey.server.wadl.internal.WadlResource.class);
+        resources.add(rest.AddressResource.class);
+        resources.add(rest.CityInfoResource.class);
+        resources.add(rest.HobbyResource.class);
+        resources.add(rest.PersonResource.class);
+        resources.add(rest.PhoneResource.class);
     }
     
 }
