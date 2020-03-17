ae08fdf2b948d4119622bf8033c0227f8b97f81f
MartinBrandstrup <martin.l.brandstrup@gmail.com>
2020-03-17 16:14:13 +0100

    Finished all REST endpoints (and tested in Postman)

--- src/main/java/rest/HobbyResource.java
+++ src/main/java/rest/HobbyResource.java
@@ -12,6 +12,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -34,10 +35,7 @@ public class HobbyResource
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
 
     @GET
-    @Produces(
-            {
-                MediaType.APPLICATION_JSON
-            })
+    @Produces(MediaType.APPLICATION_JSON)
     public String demo()
     {
         return "{\"msg\":\"Hello World\"}";
@@ -45,10 +43,7 @@ public class HobbyResource
 
     @Path("count")
     @GET
-    @Produces(
-            {
-                MediaType.APPLICATION_JSON
-            })
+    @Produces(MediaType.APPLICATION_JSON)
     public String getHobbyCount()
     {
         long count = FACADE.getHobbyCount();
@@ -110,7 +105,7 @@ public class HobbyResource
         int deletedId = deletedHobby.getId();
         return "{\"msg\":\"Hobby with id " + deletedId + " has been deleted\"}";
     }
-    
+
     @DELETE
     @Path("/delete/{id}")
     @Produces(MediaType.APPLICATION_JSON)
@@ -118,6 +113,17 @@ public class HobbyResource
     {
         FACADE.deleteHobbyById(id);
         return "{\"msg\":\"Hobby with id " + id + " has been deleted\"}";
+    }
+
+    @PUT
+    @Path("/edit/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String editHobby(@PathParam("id") int id, String hobby)
+    {
+        Hobby h = GSON.fromJson(hobby, Hobby.class);
+        Hobby editedHobby = FACADE.editHobby(id, h);
+        return GSON.toJson(new HobbyDTO(editedHobby));
     }
 
     @POST
