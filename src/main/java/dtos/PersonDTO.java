8e061493b515bd89fe31a262e2bad46db044766a
MartinBrandstrup <martin.l.brandstrup@gmail.com>
2020-03-18 11:29:19 +0100

    Fixed missing values and checks in PersonDTO

--- src/main/java/dtos/PersonDTO.java
+++ src/main/java/dtos/PersonDTO.java
@@ -18,16 +18,21 @@ public class PersonDTO
 {
 
     private int id;
-    private String firstName, lastName, street, city, zip;
+    private String firstName, lastName, email, street, city, zip;
     private List<String> phones, hobbies;
 
     public PersonDTO(Person person)
     {
         this.id = person.getId();
         this.firstName = person.getFirstName();
-        this.street = person.getAddress().getStreet();
-        this.city = person.getAddress().getCityInfo().getCity();
-        this.zip = String.valueOf(person.getAddress().getCityInfo().getZipCode());
+        this.lastName = person.getLastName();
+        this.email = person.getEmail();
+        if (person.getAddress() != null)
+        {
+            this.street = person.getAddress().getStreet();
+            this.city = person.getAddress().getCityInfo().getCity();
+            this.zip = String.valueOf(person.getAddress().getCityInfo().getZipCode());
+        }
 
         if (person.getHobbies() != null)
         {
