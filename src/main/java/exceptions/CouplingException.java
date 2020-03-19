5f8ce0cc38bcc7b25ed985b119b76caff991d561
Claus Mikkelsen Findinge <clamifi@hotmail.com>
2020-03-19 21:56:08 +0100

    Create CouplingException.java

---
+++ src/main/java/exceptions/CouplingException.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exceptions;
+
+/**
+ *
+ * @author Brandstrup
+ */
+public class CouplingException extends Exception
+{
+
+    public CouplingException(String msg)
+    {
+        super(msg);
+    }
+    
+}
