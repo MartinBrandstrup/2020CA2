c2e6f439716a072d89ee4ad420e4b30a7708a7e3
CaKehr <cph-ck209@cphbusiness.dk>
2020-03-22 09:44:36 +0100

    CountpersonsWIthHobby
    Add: <div id count> >/div> to place count
    Add: countPersonWithHobbies(id)
    Add error section for js fetch (did not implemtet yet)

--- src/main/webapp/JavaScript/table.js
+++ src/main/webapp/JavaScript/table.js
@@ -4,19 +4,22 @@
  * and open the template in the editor.
  */
 
-
+//attributs
 var URLTest = "http://localhost:8080/2020CA2/api/person/all"
-var URL = "/2020CA2/api/person"
 var URLAll = "/2020CA2/api/person/all"
 var URLpvh = "/2020CA/api/person/PersonsWithHobby/"
 var URLpvhc =  "/2020CA/api/person/countPersonsWithHobby/" 
 const fillBtn = document.getElementById("loadPersons");
 const chosenHobby = document.getElementById("txt")
 
-
+//events
 fillBtn.addEventListener("click", fillTable, false )
 chosenHobby.addEventListener("change", eventHandler, false )
 
+/**
+ * 
+ * @returns {All persons for table from api}
+ */
 function fillTable(){
 fetch(URLAll)
         .then(res => res.json())
@@ -33,7 +36,12 @@ fetch(URLAll)
 });
 }
 
-function fillTableWithHobbies(id){
+/**
+ * 
+ * @param {type} id
+ * @returns {table data for all persons with given hoppy id}
+ */
+function personsWithHobbies(id){
     fetch(URLpvh + id).then(res => res.json())
         .then(data =>{
         let list = data.map(function(person){
@@ -45,40 +53,43 @@ function fillTableWithHobbies(id){
                         "</tr>";
             }).join("");
             document.getElementById("indexTabelBody").innerHTML = list;
+        // next time il use promises        
+        countPersonWithHobbies(id);
 });
 }
-
+/**
+ * 
+ * @param {type} id
+ * @returns {count of persons with given hobby}
+ */
+function countPersonWithHobbies(id){
+    fetch(URLpvh + id).then(res => res.json())
+        .then(data =>{ 
+            document.getElementById("count").innerHTML = data;
+        
+           
+            
+});
+}
+/*
+ * Manage event and confirm data in development fase
+ */
 function eventHandler(e) {
 // tag id givet fra event 
   var hobbyId = e.target.value;
   console.log(hobbyId);
   
-    fillTableWithHobbies(hobbyId);
+    personsWithHobbies(hobbyId);
+    
   }
  // getCountry henter daten og retuner det til returnPoints 
  
 
-//function fillTable(e){
-//    console.log(id)  
-//if (id == "svg2"){
-//    returnPoint1.innerText = "Name: Ocean" 
-//    returnPoint2.innerText = "Population: alot " 
-//    returnPoint3.innerText = "Area: less than 510.100.000 km2"
-//    returnPoint4.innerText = "Borders: incuding rivers? idk!"  
-//  } else {
-//
-//    var url = "http://restcountries.eu/rest/v1/alpha?codes=" + id
-//    console.log(url)
-//    fetch(url)
-//    .then(res => res.json())
-//    .then(data => {
-//      // kontrol af data indhold
-//        console.log(data)
-//      // then data =>{} = hvad skal der ske med dataen    
-//        returnPoint1.innerText = "Name: " + data[0].name
-//        returnPoint2.innerText = "Population: " + data[0].population
-//        returnPoint3.innerText = "Area: " + data[0].area
-//        returnPoint4.innerText = "Borders: " + data[0].borders
-//  })}
+//error handling if time
+//fetch (...).then( res => {
+//    if (res.ok) {
+//        console.log("Success")
+//    }else {
+//        console.log("Not successfull")
+//    }
 //}
-
